89b02c0c5916198e887e38648c12805ff8ebc150
Andrew Williams <apwilliams508@gmail.com>
2023-12-28 13:39:41 +0000

    thing

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package checks
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -24,40 +22,15 @@ func NewCheck(name string, function Function) {
 }
 
 func RegisterChecks() {
-	//  loop checks
 	for _, check := range checks {
-
-		// compare check.name with env var "CHECK_ACTION"
 		if check.name == os.Getenv("CHECK_ACTION") {
-
-			// create post body var equal equal to
 			postBody, _ := json.Marshal(check.function())
-
-			// string res to buf
 			requestBody := bytes.NewBuffer(postBody)
-
-			// post to callback url (env var) with headxer format of json and that req.body is req.body (buf)
 			resp, err := http.Post(os.Getenv("CHECK_CALLBACK_URL"), "application/json", requestBody)
-
-			//
 			if err != nil {
 				log.Fatalf("An Error Occured %v", err)
 			}
-
-			//
 			defer resp.Body.Close()
-
-			//
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatalln(err)
-			}
-
-			//
-			sb := string(body)
-			fmt.Println(sb)
-
 		}
 	}
-	//
 }
