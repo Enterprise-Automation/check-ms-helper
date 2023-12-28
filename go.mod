0e645cfdc730cae6332ad0dd78ca1f3c0b8df5f5
admin <kalem@enterpriseautomation.co.uk>
2023-12-28 12:14:12 +0000

    git add

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Enterprise-Automation/check-ms-helper
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+}
+
+func NewCheck(name string) {
+	fmt.Println("New Check", name)
+}
+
+func RegisterChecks() {
+	fmt.Println("Register")
+}
