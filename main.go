7047f104dcc381ed770dc332f3be115b43fa946b
Andrew Williams <apwilliams508@gmail.com>
2023-12-28 12:55:42 +0000

    thing

--- main.go
+++ main.go
@@ -6,11 +6,20 @@ import (
 
 type Function func() interface{}
 
+type check struct {
+	name     string
+	function Function
+}
+
+var checks []check
+
 func NewCheck(name string, function Function) {
 	fmt.Println("New Check", name)
-	function()
+	checks = append(checks, check{name: name, function: function})
 }
 
 func RegisterChecks() {
-	fmt.Println("Register")
+	// thingToCheck := "check2"
+	// call the right function
+	fmt.Println(checks)
 }
