7cc8322897b355621141ae827e3db004f9639672
admin <kalem@enterpriseautomation.co.uk>
2024-01-04 13:38:28 +0000

    Changes to allow CTX (context.Context) to be provided as an arg to helper functions

--- main.go
+++ main.go
@@ -7,28 +7,30 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"context"
 )
 
-type Function func() map[string]interface{}
+type Function func(ctx context.Context) map[string]interface{}
 
 type check struct {
 	name     string
 	function Function
+	item	context.Context
 }
 
 var checks []check
 
-func NewCheck(name string, function Function) {
+func NewCheck(name string, function Function, item context.Context) {
 	fmt.Println("New check: " + name)
-	checks = append(checks, check{name: name, function: function})
+	checks = append(checks, check{name: name, function: function, item: item})
 }
 
 func RegisterChecks() {
 	fmt.Println("registering checks")
 	for _, check := range checks {
 		if check.name == os.Getenv("CHECK_ACTION") {
-			fmt.Println("Performing check: " + check.name)
-			postBody, _ := json.Marshal(check.function())
+			fmt.Println("Performing check: " + check.name )
+			postBody, _ := json.Marshal(check.function(check.item))
 			requestBody := bytes.NewBuffer(postBody)
 			fmt.Println("making request to: " + os.Getenv("CHECK_CALLBACK_URL"))
 			fmt.Println("with body: \n" + string(postBody))
